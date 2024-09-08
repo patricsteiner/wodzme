9149b8de874095d858c3b43573a6e2b7c6d180b3
Patric Steiner <patric.steiner1@gmail.com>
2024-09-08 19:01:53 +0200

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
+
+const firebaseOptions = {"projectId":"wodzme","appId":"1:497002003415:web:738026de8196ab42e46992","storageBucket":"wodzme.appspot.com","apiKey":"AIzaSyDF77iFBbdcCxq1J3-b9LjvIjXO4NKh6OM","authDomain":"wodzme.firebaseapp.com","messagingSenderId":"497002003415"}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp(firebaseOptions)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideVertexAI(() => getVertexAI())]
 };
